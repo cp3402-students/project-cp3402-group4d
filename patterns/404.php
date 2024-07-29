7786e1c1794dd388db6fe821eec28f2fbaa64b2a
Zhenqi Ma <128345606+Byeclouds@users.noreply.github.com>
2024-07-29 19:57:22 +0800

    a2_step_2

---
+++ patterns/404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: 404
+ * Slug: groupD/404
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/1600x512px_About-us_header.jpg","dimRatio":50,"minHeight":40,"minHeightUnit":"vw","layout":{"type":"default"}} -->
+<div class="wp-block-cover" style="min-height:40vw"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background " alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/1600x512px_About-us_header.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","style":{"typography":{"fontSize":"70px"},"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}}} -->
+<p class="has-text-align-center" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);font-size:70px"><strong>404</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Page not found.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
---
+++ patterns/single.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: single
+ * Slug: groupD/single
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/1600x512px_About-us_header.jpg","dimRatio":50,"layout":{"type":"default"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background " alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/1600x512px_About-us_header.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:post-title {"textAlign":"center"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:post-date {"textAlign":"center"} /--></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
+
+<!-- wp:post-content /-->
+
+<!-- wp:template-part {"slug":"footer","area":"footer"} /-->
